e9a0dde0a4fad121d7c351c7c8ed0f4fc41987cd
Fellipe Bastos <dev@fellipebastos.com>
2019-03-14 01:02:38 -0300

    Adiciona slot ao componente card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,8 @@
 <div class="card border-0 shadow-sm">
+    <div class="card-header border-0 font-weight-bold shadow-sm">
+        {{ $header ?? '' }}
+    </div>
+
     <div class="card-body">
         {{ $slot }}
     </div>
