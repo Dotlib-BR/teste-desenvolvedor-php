f79be2235a3ce89ffa16fea51bd4f08e6a1f358c
thulioprado <thutnaweb@gmail.com>
2019-07-01 00:00:39 -0300

    Removing dead code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,30 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('*', function($view) {
-            $name = explode('.', $view->getName());
-
-            if (count($name) > 1) {
-                $names = [
-                    'users'    => ['Usuário', 'Usuários'],
-                    'products' => ['Produto', 'Produtos'],
-                    'orders'   => ['Pedido', 'Pedidos']
-                ];
-
-                view()->share('route', $name[0]);
-
-                if (array_key_exists($name[0], $names)) {
-                    view()->share('singular', $names[$name[0]][0]);
-                    view()->share('plural', $names[$name[0]][1]);
-                } else {
-                    view()->share('singular', 'unknown');
-                    view()->share('plural', 'unknown');
-                }
-            } else {
-                view()->share('route', 'unknown');
-            }
-        });
-
         validator()->extend('cpf', function($attribute, $value, $parameters, $validator) {
             $cpf = preg_replace( '/[^0-9]/is', '', $value);
 
