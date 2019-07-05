8865ca5e7639e028f369514dcddfa92b08d32d06
Vlademir Junior <vlademirjunior@protonmail.com>
2019-07-04 21:50:14 -0300

    Feito organização dos testes em diretórios separados

---
+++ teste/tests/Feature/Dashboard/ClientTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Dashboard;
+
+use Tests\TestCase;
+
+class ClientTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
--- teste/tests/Feature/Zeus/ClientTest.php
+++ teste/tests/Feature/Zeus/ClientTest.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Zeus;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ClientTest extends TestCase
 {
