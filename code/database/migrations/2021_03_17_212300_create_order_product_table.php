425371f14dd8c70aae947654fdc06aa46311a228
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-17 19:38:20 -0300

    add(order-product-migration): add order-product migration

---
+++ code/database/migrations/2021_03_17_212300_create_order_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("order_id");
+            $table->bigInteger("product_id");
+            $table->integer("quantity");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+}
