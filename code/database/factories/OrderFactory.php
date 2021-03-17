76bb0cddbd92d74d017cd7ea19067cc0c49cbd96
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-17 19:39:45 -0300

    add(order-factory): add order factory

--- code/database/factories/OrderFactory.php
+++ code/database/factories/OrderFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OrderFactory extends Factory
@@ -22,7 +23,28 @@ class OrderFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "date" => $this->faker->dateTime("now","America/Sao_Paulo"),
+            "status" => $this->faker->randomElement([
+                "opened",
+                "paid_out",
+                "canceled"
+            ]),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Order $order) {
+            $products  = Product::factory(2)->create();
+            $products->each(function ($product) use ($order) {
+                return $order->products()->attach($product->id, [
+                    "quantity" => $this->getDiscountedQuantity($product->quantity)
+                ]);
+            });
+        });
+    }
+
+    private function getDiscountedQuantity($quantity, $percent = 0.2) {
+        return $quantity * $percent;
     }
 }
