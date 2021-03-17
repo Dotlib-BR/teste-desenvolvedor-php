dbb475b05dced6e125f3dcece94f22042729db1e
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-17 19:45:22 -0300

    add(client-seeder: add client seeder

--- code/database/seeders/ClientSeeder.php
+++ code/database/seeders/ClientSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Client;
 
 class ClientSeeder extends Seeder
 {
@@ -13,6 +14,6 @@ class ClientSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Client::factory(5)->create();
     }
 }
