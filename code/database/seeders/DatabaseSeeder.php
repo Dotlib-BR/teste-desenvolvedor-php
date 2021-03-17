4d324c944a44953508b262553cb35fc214d7d1b8
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-17 19:46:13 -0300

    add(seeder): call seeders in database seeder

--- code/database/seeders/DatabaseSeeder.php
+++ code/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ClientSeeder::class,
+            ProductSeeder::class,
+            OrderSeeder::class,
+        ]);
     }
 }
