d0e82beaac3c1837a65ace8026aa2fe72c2ecd59
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-17 19:38:55 -0300

    add(product-model): add product model

--- code/app/Models/Product.php
+++ code/app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name", "quantity", "price", "bar_code"
+    ];
+
+    public function orders() {
+        return $this->belongsToMany(Order::class)->withPivot("quantity");
+    }
 }
