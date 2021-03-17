45590e17166227198651517edb0544564efde711
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-17 19:39:04 -0300

    add(order-model): add order model

--- code/app/Models/Order.php
+++ code/app/Models/Order.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "date", "status"
+    ];
+
+    protected $casts = [
+        "date" => "datetime"
+    ];
+
+    public function products() {
+        return $this->belongsToMany(Product::class)->withPivot("quantity");
+    }
 }
