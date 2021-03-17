a83eddad74e2037d52468e23c68b9337d9a0b7ab
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-17 19:37:59 -0300

    add(orders-migration): add columns in orders migration

--- code/database/migrations/2021_03_17_211322_create_orders_table.php
+++ code/database/migrations/2021_03_17_211322_create_orders_table.php
@@ -15,6 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string("status");
+            $table->datetime("date");
+            $table->bigInteger("client_id")->nullable(); //nullable only to factories
             $table->timestamps();
         });
     }
