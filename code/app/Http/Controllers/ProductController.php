6c29cc97a89812f6b24789da43f8a1dfd75a31eb
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-18 18:59:20 -0300

    add(show-product-controller): add show method in product controller

--- code/app/Http/Controllers/ProductController.php
+++ code/app/Http/Controllers/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view("product.show", ["product" => $product]);
     }
 
     /**
