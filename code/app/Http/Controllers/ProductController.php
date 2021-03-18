ff8e2138ad7ff9e4b8856e358105d99944964945
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-18 18:52:23 -0300

    add(index-product-controller): add index method in product controller

--- code/app/Http/Controllers/ProductController.php
+++ code/app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return view("product.index", ["products" => Product::all()]);
     }
 
     /**
