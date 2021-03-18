daf5ac7683544867d8b4461cd43bc268a2072214
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-03-18 10:18:20 -0300

    role_id nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(2);
+            $table->unsignedBigInteger('role_id')->default(2)->nullable();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
