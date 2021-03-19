dbfa27cea358d810421690219eccd7e8c853ee2c
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-19 19:37:16 -0300

    refactor(create-client-view): add tailwind css in create client view

--- code/resources/views/client/create.blade.php
+++ code/resources/views/client/create.blade.php
@@ -1,16 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clientes</title>
-</head>
-<body>
-    <form method="POST" action="{{ route('client.create') }}">
-        <input type="text" name="name">
-        <input type="email" name="email">
-        <input type="text" name="cpf">
-        <button type="submit">Enviar</button>
-    </form>
-</body>
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Cadastrar novo Cliente') }}
+        </h2>
+        <nav class="bg-grey-light rounded font-sans">
+            <ol class="list-reset flex text-grey-dark">
+              <li><a href="{{ route('dashboard') }}" class="text-indigo-500 font-bold">Dashboard</a></li>
+              <li><span class="mx-2">/</span></li>
+              <li><a href="{{ route('client.index') }}" class="text-indigo-500 font-bold">Clientes</a></li>
+              <li><span class="mx-2">/</span></li>
+              <li class="text-indigo-400">Cadastrar Cliente</li>
+            </ol>
+          </nav>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form class="flex flex-col space-y-3" method="POST" action="{{ route('client.store') }}">
+                        @csrf
+                        <input required placeholder="Nome do cliente" type="text" name="name">
+                        <input required placeholder="E-mail do cliente" type="email" name="email">
+                        <input required placeholder="CPF do cliente" type="text" name="cpf">
+                        <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 px-3 py-2 rounded text-white focus:outline-none">Salvar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
