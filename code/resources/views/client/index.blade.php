c34a6423e61ce7e944317b46ce47915224ffd02a
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-19 19:36:27 -0300

    add(breadcrumbs-index-client-view): add breadcrumbs in indec client view

--- code/resources/views/client/index.blade.php
+++ code/resources/views/client/index.blade.php
@@ -3,6 +3,13 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Clientes') }}
         </h2>
+        <nav class="bg-grey-light rounded font-sans">
+          <ol class="list-reset flex text-grey-dark">
+            <li><a href="{{ route('dashboard') }}" class="text-indigo-500 font-bold">Dashboard</a></li>
+            <li><span class="mx-2">/</span></li>
+            <li class="text-indigo-400">Clientes</li>
+          </ol>
+        </nav>
     </x-slot>
 
     <div class="py-12">
@@ -10,7 +17,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 flex flex-col">
                     <div class="w-full flex flex-row-reverse mb-2.5">
-                        <a href="{{ route('client.create') }}" class="bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded text-white focus:outline-none">Criar novo Cliente</a>
+                        <a href="{{ route('client.create') }}" class="bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded text-white focus:outline-none">Cadastrar novo Cliente</a>
                     </div>
                     <table class="table-auto rounded border-b border-gray-200 min-w-full">
                         <thead class="bg-gray-800 text-white">
@@ -40,7 +47,7 @@
                             </tr>
                           @endforelse
                         </tbody>
-                      </table>
+                    </table>
                 </div>
             </div>
         </div>
