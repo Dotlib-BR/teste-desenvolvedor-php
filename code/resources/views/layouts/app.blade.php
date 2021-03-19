a0f4cd0b980e6430c6681d44f873772d4d9ee74a
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-19 19:35:57 -0300

    add(breadcrumbs-app): add nav breadcrumbs class in layout app

--- code/resources/views/layouts/app.blade.php
+++ code/resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex flex-wrap flex-row justify-between">
                     {{ $header }}
                 </div>
             </header>
