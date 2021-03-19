eee2d9b8d48a4148f8b2d0100befc678159c2250
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-19 18:04:04 -0300

    add resource routes again

--- code/routes/web.php
+++ code/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    "client" => ClientController::class,
+    "product" => ProductController::class,
+    "order" => OrderController::class
+]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
