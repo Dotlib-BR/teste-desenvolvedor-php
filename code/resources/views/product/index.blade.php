80d87f2a92da87577f477dc9e510a99c41d75ce0
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-20 16:59:22 -0300

    add(mult-delete-product): add mult delete product in index view

--- code/resources/views/product/index.blade.php
+++ code/resources/views/product/index.blade.php
@@ -18,6 +18,17 @@
               <div class="p-6 bg-white border-b border-gray-200 flex flex-col">
                   <div class="w-full flex flex-row-reverse mb-2.5">
                       <a href="{{ route('product.create') }}" class="bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded text-white focus:outline-none">Cadastrar novo Produto</a>
+                      <button
+                        disabled
+                        id="button-mult" 
+                        onclick="event.preventDefault(); if(confirm('Tem certeza que deseja deletar os clientes selecionados?')) { document.getElementById('destroy-form-mult').submit(); }"
+                        class="mx-1 bg-red-400 hover:bg-red-600 px-3 py-1 rounded text-white focus:outline-none"
+                        >Deletar Selecionados
+                      </button>
+                      <form id="destroy-form-mult" action="{{ route('product.multDestroy') }}" method="POST" style="display: none;">
+                        @csrf
+                        @method('DELETE')
+                      </form>  
                   </div>
                   <div class="w-full py-1">
                     <table id="table" class="stripe hover">
@@ -28,6 +39,7 @@
                           <th data-priority="3" class="text-center">Quantidade</th>
                           <th data-priority="4" class="text-center">Proço unitario</th>
                           <th class="text-center">Ação</th>
+                          <th class="text-center">Selecionar</th>
                         </tr>
                       </thead>
                       <tbody>
@@ -47,16 +59,18 @@
                                       @csrf
                                       @method('DELETE')
                                   </form>
-
+                              </td>
+                              <td class="text-center">
+                                <input value="{{ $product->id }}" m-type="product" class="checkbox" type="checkbox">  
                               </td>
                           </tr>
                         @empty
                           <tr>
-                              <td class="text-center" colspan="5">Sem Ps cadastrados</td>
+                              <td class="text-center" colspan="6">Sem Ps cadastrados</td>
                           </tr>
                         @endforelse
                       </tbody>
-                  </table>
+                    </table>
                   </div>
               </div>
           </div>
