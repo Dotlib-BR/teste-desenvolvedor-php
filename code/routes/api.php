8fd9241d8fd79e90064867cb73c8185faa67a1fd
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-20 19:02:12 -0300

    refactor(api-routes): add named group in resources api routes

--- code/routes/api.php
+++ code/routes/api.php
@@ -21,8 +21,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResources([
-    "client" => ClientController::class,
-    "product" => ProductController::class,
-    "order" => OrderController::class
-]);
+Route::name('api.')->group(function () {
+    Route::apiResources([
+        "client" => ClientController::class,
+        "product" => ProductController::class,
+        "order" => OrderController::class
+    ]);
+});
