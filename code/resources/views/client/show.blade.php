277273de799931cd66685f2ec4992069e287c5b3
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-20 11:58:39 -0300

    add(show-client): add tailwind in show client view

--- code/resources/views/client/show.blade.php
+++ code/resources/views/client/show.blade.php
@@ -1,11 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clientes</title>
-</head>
-<body>
-    {{$client}}
-</body>
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Visualizar Cliente') }}
+        </h2>
+        <nav class="bg-grey-light rounded font-sans">
+            <ol class="list-reset flex text-grey-dark">
+              <li><a href="{{ route('dashboard') }}" class="text-indigo-500 font-bold">Dashboard</a></li>
+              <li><span class="mx-2">/</span></li>
+              <li><a href="{{ route('client.index') }}" class="text-indigo-500 font-bold">Clientes</a></li>
+              <li><span class="mx-2">/</span></li>
+              <li class="text-indigo-400">Visualizar Cliente</li>
+            </ol>
+          </nav>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex flex-col space-y-3">
+                        <input value="{{ $client->name }}" disabled type="text">
+                        <input value="{{ $client->email }}" disabled type="text">
+                        <input value="{{ $client->cpf }}" disabledt type="text">
+                        <a href="{{ route('client.edit', $client) }}" class="text-center bg-yellow-500 hover:bg-yellow-600 px-3 py-2 rounded text-white focus:outline-none">Editar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
