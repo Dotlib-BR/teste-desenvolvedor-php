507e72120fe5ea699bd539137fc15be9b6f3880c
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-20 18:47:37 -0300

    add(api-routes): add api resources routes

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    "client" => ClientController::class,
+    "product" => ProductController::class,
+    "order" => OrderController::class
+]);
