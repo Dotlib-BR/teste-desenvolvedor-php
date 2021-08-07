d462439dd8869a7e9db011c135ba3f569dc9754e
Matheus <theusrsilva@gmail.com>
2021-08-07 02:31:54 -0300

    tradução pt-br

--- resources/views/cruds/clientes/create.blade.php
+++ resources/views/cruds/clientes/create.blade.php
@@ -5,15 +5,15 @@
             @csrf
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Nome</label>
-            <input name='nome' class="form-control"  >
+            <input name='nome' class="form-control" value="{{old('nome')}} >
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">CPF</label>
-            <input name="cpf" class=" cpf form-control"  >
+            <input name="cpf" class=" cpf form-control" value="{{old('cpf')}}" >
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">E-mail</label>
-            <input name="email" type="email" class="form-control" >
+            <input name="email" type="email" class="form-control" value="{{old('email')}}>
         </div>
             <div class="col-5">
                 <button type="submit" class="btn btn-dark">Enviar</button>
