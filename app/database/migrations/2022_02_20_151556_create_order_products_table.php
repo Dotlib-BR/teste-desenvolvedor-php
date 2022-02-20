ea5c5841f9cbe4b407e9b6a5e2f0989f461eebb5
Henri Borges <henriborgessilva@gmail.com>
2022-02-20 18:21:51 -0300

    feat: create migrations

---
+++ app/database/migrations/2022_02_20_151556_create_order_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->foreignId('product_id');
+            $table->integer('quantity');
+            $table->double('unit_price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+    }
+}
