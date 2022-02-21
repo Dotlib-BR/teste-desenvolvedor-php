5374ecc8746a99869c59b25754a258da51ca728a
Henri Borges <henriborgessilva@gmail.com>
2022-02-20 22:03:37 -0300

    feat: create clients resource api

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function() {
-    return 'yoo';
-});
+Route::resource('/clients', ClientController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
