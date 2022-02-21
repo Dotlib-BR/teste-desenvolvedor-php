cd18cbd006e2aedc791b9744a8d3f2a7dde08e25
Henri Borges <henriborgessilva@gmail.com>
2022-02-21 16:16:02 -0300

    feat: create relation between client and orders

--- app/app/Models/Order.php
+++ app/app/Models/Order.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Order extends Model
 {
@@ -20,5 +23,10 @@ class Order extends Model
     public function orderProducts(): HasMany
     {
         return $this->hasMany(OrderProduct::class);
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
     }
 }
