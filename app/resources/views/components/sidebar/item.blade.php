317a4086982cbfc3fe2d7ac6084fc07d6a8353c3
Henri Borges <henriborgessilva@gmail.com>
2022-02-22 17:04:27 -0300

    style: change sidebar items color

--- app/resources/views/components/sidebar/item.blade.php
+++ app/resources/views/components/sidebar/item.blade.php
@@ -3,7 +3,7 @@
     'url',
 ])
 
-<a href="{{ $url }}" class="transition ease-in-out delay-25 flex items-center gap-4 rounded-lg {{ Route::current()->uri === $url ? "bg-green-400 hover:bg-green-500" : "hover:bg-gray-100" }} -m-1 p-1">
+<a href="{{ $url }}" class="transition ease-in-out delay-25 flex items-center gap-4 rounded-lg {{ Route::current()->uri === $url ? "bg-green-500 hover:bg-green-400 text-white" : "hover:bg-gray-100" }} -m-1 p-1">
     <div class="relative h-6 w-6 text-center">
         <i class="fa-solid fa-{{ $icon }} fa-lg"></i>
     </div>
