8c72702a83e74dbfe5e513e8f7045de3ff906925
Henri Borges <henriborgessilva@gmail.com>
2022-02-22 17:07:47 -0300

    fix: use mix to track app.css

--- app/resources/views/layouts/main.blade.php
+++ app/resources/views/layouts/main.blade.php
@@ -5,15 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <div class="flex flex-row w-screen h-screen">
-
-        <x-sidebar.sidebar />
+        <div>
+            <x-sidebar.sidebar />
+        </div>
         
-        <section id="main-content" class="bg-gray-100 w-full">
+        <section id="main-content" class="bg-gray-100 w-full overflow-auto">
             <div class="border-b h-[4rem] flex justify-between px-6 items-center">
                 <div class="text-sm font-medium">
                     {{ $pageName }}
@@ -21,6 +22,9 @@
                 @yield('header')
             </div>
             <div class="mt-10 px-4 sm:px-6 md:px-8">
+                <div class="mb-10">
+                    <span class="font-bold text-2xl md:text-3xl tracking-tight">{{ $pageName }}</span>
+                </div>
                 @yield('content')
             </div>
         </section>
