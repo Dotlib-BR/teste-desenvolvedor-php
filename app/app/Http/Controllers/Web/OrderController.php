32c55f0b160be6575d8917ecd69caa00e8b40dc6
Henri Borges <henriborgessilva@gmail.com>
2022-02-23 15:11:29 -0300

    feat: create order controller and index method

---
+++ app/app/Http/Controllers/Web/OrderController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $request->validate([
+            'per_page' => 'integer',
+            'search_params',
+        ]);
+
+
+        if(session('success_message')) {
+            Alert::success('Sucesso!', session('success_message'));
+        }
+
+        $per_page = $request->input('per_page') ?: 10;
+        $search_params = $request->input('search_params');
+
+        if ($search_params) {
+            $orders = Order::advancedSearch($search_params)->paginate($per_page);
+        } else {
+            $orders = Order::paginate($per_page);
+        }
+
+        return view('orders.index', [
+            'orders' => $orders,
+            'per_page' => $per_page,
+            'searchable' => true,
+            'search_params' => $search_params
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
