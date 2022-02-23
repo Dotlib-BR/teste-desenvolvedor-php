46acfed85c27eb862c349a95ed3e54c532ade1e2
Henri Borges <henriborgessilva@gmail.com>
2022-02-23 15:10:59 -0300

    feat: implement order advanced search and total value attribute

--- app/app/Models/Order.php
+++ app/app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Contracts\AdvancedSearchable;
 use App\Enums\OrderStatus;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
-class Order extends Model
+class Order extends Model implements AdvancedSearchable
 {
     use HasFactory;
 
@@ -28,5 +29,17 @@ class Order extends Model
     public function client(): BelongsTo
     {
         return $this->belongsTo(Client::class);
+    }
+
+    public function getTotalValueAttribute()
+    {
+        return $this->orderProducts()->sum('unit_price');
+    }
+
+    public static function scopeAdvancedSearch($query, $param)
+    {
+        $query->join('clients', 'clients.id', 'orders.client_id')
+            ->select('clients.name', 'orders.*')
+            ->where('clients.name', 'like', "%$param%");
     }
 }
