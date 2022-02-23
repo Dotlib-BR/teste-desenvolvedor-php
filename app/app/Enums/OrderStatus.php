e5f388bdbd00acda1a0d7981eada3e270424e6ff
Henri Borges <henriborgessilva@gmail.com>
2022-02-23 15:11:16 -0300

    feat: create status custom description

--- app/app/Enums/OrderStatus.php
+++ app/app/Enums/OrderStatus.php
@@ -10,4 +10,27 @@ final class OrderStatus extends Enum
     const Production = 1;
     const Shipped = 2;
     const Delivered = 3;
+
+    public static function getDescription($value): string
+    {
+        switch($value) {
+            case self::Ordered:
+                return "Pedido";
+                break;
+
+            case self::Production:
+                return "Em produção";
+                break;
+
+            case self::Shipped:
+                return "Enviado";
+                break;
+
+            case self::Delivered:
+                return "Entregue";
+                break;
+        }
+        
+        return parent::getDescription($value);
+    }
 }
