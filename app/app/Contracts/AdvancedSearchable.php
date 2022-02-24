1ac4bb8a9598cbade861ecc60df91c77c33487d0
Henri Borges <henriborgessilva@gmail.com>
2022-02-23 23:26:00 -0300

    feat: create advanced searchable display methods

--- app/app/Contracts/AdvancedSearchable.php
+++ app/app/Contracts/AdvancedSearchable.php
@@ -5,4 +5,8 @@ namespace App\Contracts;
 interface AdvancedSearchable
 {
     public static function scopeAdvancedSearch($query, $param);
+
+    public function listedFormat();
+
+    public function displayResultFormat();     
 }
