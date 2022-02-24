3c44688e7282e85e00d775aaf06da8ed8a98f15e
Henri Borges <henriborgessilva@gmail.com>
2022-02-23 23:49:09 -0300

    style: replace system name at sidebar by dot.lib

--- app/resources/views/components/sidebar/sidebar.blade.php
+++ app/resources/views/components/sidebar/sidebar.blade.php
@@ -1,7 +1,9 @@
 <section id="sidebar" class="w-[320px] h-full relative">
     <div class="absolute w-full h-full bg-white z-50" style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;">
         <div class="border-b h-[4rem] flex items-center px-6">
-            <h1 class="font-bold text-xl tracking-tight">Meu Sistema</h1>
+            <div>
+                <span class="text-green-500 text-3xl font-bold tracking-tight">dot.</span><span class="text-black text-3xl font-bold tracking-tight">lib</span>
+            </div>
         </div>
         <div class="px-10 mt-4 flex flex-col">
             <span class="tracking-wide text-xs text-gray-600 font-bold mb-4">LOJA</span>
