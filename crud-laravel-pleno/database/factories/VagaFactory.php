4e28e528892c2dbebd7a1b79bc1bf6d3b7c522ed
thiagosoares <thiagocsoares@gmail.com>
2023-08-28 09:33:22 -0300

    criando factory vaga

--- crud-laravel-pleno/database/factories/VagaFactory.php
+++ crud-laravel-pleno/database/factories/VagaFactory.php
@@ -17,7 +17,12 @@ class VagaFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'titulo' => fake()->sentence, // Uma descrição curta para o titulo
+            'descricao' => fake()->paragraph, // Paragrafo
+            'tipo' => fake()->randomElement(['CLT', 'Pessoa Jurídica', 'Freelancer']), // Tipo de vaga
+            'status' => fake()->randomElement(['Ativa', 'Pausada', 'Encerrada']), // Status da vaga
+            'created_at' => fake()->dateTimeBetween('-1 year', 'now'), // Data, sendo criada 12 meses atrás a partir da hora de criação
+            'updated_at' => now(), // Data modificação da vaga           
         ];
     }
 }
