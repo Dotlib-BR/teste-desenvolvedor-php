a9e6229c0fbefabd66401ff75ec88a1923466206
thiagosoares <thiagocsoares@gmail.com>
2023-08-30 16:58:17 -0300

    adicionando rotas para autenticacao

--- crud-laravel-pleno/routes/web.php
+++ crud-laravel-pleno/routes/web.php
@@ -5,17 +5,14 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\VagaController;
 use App\Http\Controllers\InscricaoController;
 use App\Http\Controllers\CandidatoController;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+
 Route::prefix('vagas')->group(function () {
     Route::get('/', [VagaController::class, 'index']);
     Route::post('/', [VagaController::class, 'store']);
