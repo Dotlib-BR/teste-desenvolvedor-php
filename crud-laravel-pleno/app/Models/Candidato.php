acf91830defd08ef2a966e0d52b183c9044ed578
thiagosoares <thiagocsoares@gmail.com>
2023-08-30 16:37:38 -0300

    Alterações para MOdels Candidato para paginação e validação

--- crud-laravel-pleno/app/Models/Candidato.php
+++ crud-laravel-pleno/app/Models/Candidato.php
@@ -1,24 +1,51 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Models\Candidato;
 
-class Candidato extends Model
+class CandidatoController extends Controller
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'name',
-        'email',
-        'experiencia_profissional',
-        'habilidades',
-        'disponibilidade',
-    ];
-
-    public function inscricoes()
+    public function index()
     {
-        return $this->hasMany(Inscricao::class);
+        $candidatos = Candidato::paginate(20);
+        return response()->json($candidatos);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|unique:candidatos',
+        ]);
+
+        $candidato = Candidato::create($validatedData);
+        return response()->json($candidato, 201);
+    }
+
+    public function show($id)
+    {
+        $candidato = Candidato::findOrFail($id);
+        return response()->json($candidato);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|unique:candidatos,email,'.$id,
+        ]);
+
+        $candidato = Candidato::findOrFail($id);
+        $candidato->update($validatedData);
+        return response()->json($candidato);
+    }
+
+    public function destroy($id)
+    {
+        $candidato = Candidato::findOrFail($id);
+        $candidato->delete();
+        return response()->json(null, 204);
     }
 }
