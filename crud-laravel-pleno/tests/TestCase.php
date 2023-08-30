95cec9f03a3a3e3582810bcb990268e1dadc6592
thiagosoares <thiagocsoares@gmail.com>
2023-08-30 19:37:19 -0300

    corracao para teste case

--- crud-laravel-pleno/tests/TestCase.php
+++ crud-laravel-pleno/tests/TestCase.php
@@ -7,4 +7,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Criar teste de usuario para autenticacao.
+     *
+     * @return \App\Models\User
+     */
+    protected function createTestUser()
+    {
+        return \App\Models\User::factory()->create([
+            'email' => 'test@example.com',
+        ]);
+    }
 }
