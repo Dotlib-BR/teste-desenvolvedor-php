38df3fe2ece1974344d9e3f35a112581fa82a91f
thiagosoares <thiagocsoares@gmail.com>
2023-08-30 13:48:22 -0300

    Adicionando rotas para vaga e user

--- crud-laravel-pleno/routes/api.php
+++ crud-laravel-pleno/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VagaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::prefix('vagas')->group(function(){
+    Route::get('/', [VagaController::class, 'index']);
+    Route::post('/', [VagaController::class, 'stor']);
+    Route::get('/{vaga}', [VagaController::class, 'show']);
+    Route::pull('/{vaga}', [VagaController::class, 'update']);
+    Route::delete('/{vaga}', [VagaController::class, 'destroy']);
+});
+
+Route::prefix('user')->group(function(){
+    Route::get('/', [VagaController::class, 'index']);
+    Route::post('/', [VagaController::class, 'store']);
+    Route::get('/{user}', [VagaController::class, 'show']);
+    Route::pull('/{user}', [VagaController::class, 'update']);
+    Route::delete('/{user}', [VagaController::class, 'destroy']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
