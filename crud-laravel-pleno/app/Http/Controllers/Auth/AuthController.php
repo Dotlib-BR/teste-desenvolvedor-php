5ab0ce22a55f737d301234e3b2280ccb73c4e79c
thiagosoares <thiagocsoares@gmail.com>
2023-09-01 10:15:29 -0300

    adicionando Dockerfile e edicao do Readme.md

---
+++ crud-laravel-pleno/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use AuthenticatesUsers;
+
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
