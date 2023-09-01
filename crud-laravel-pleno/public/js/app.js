7ab2b59a8e18079c6a3e959daf9b8650002b0cde
thiagosoares <thiagocsoares@gmail.com>
2023-09-01 06:35:40 -0300

    atualizacao do nvm, node, package, webpackmix para cjs para atualizacao esm para commonjs

---
+++ crud-laravel-pleno/public/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\n/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/postcss.config.js:1\nexport default {\n^^^^^^\n\nSyntaxError: Unexpected token 'export'\n    at internalCompileFunction (node:internal/vm:73:18)\n    at wrapSafe (node:internal/modules/cjs/loader:1153:20)\n    at Module._compile (node:internal/modules/cjs/loader:1197:27)\n    at Module._extensions..js (node:internal/modules/cjs/loader:1287:10)\n    at Module.load (node:internal/modules/cjs/loader:1091:32)\n    at Module._load (node:internal/modules/cjs/loader:938:12)\n    at Module.require (node:internal/modules/cjs/loader:1115:19)\n    at module.exports (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/import-fresh/index.js:32:59)\n    at loadJs (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/loaders.js:16:18)\n    at Explorer.loadFileContent (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/Explorer.js:84:32)\n    at Explorer.createCosmiconfigResult (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/Explorer.js:89:36)\n    at Explorer.loadSearchPlace (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/Explorer.js:70:31)\n    at async Explorer.searchDirectory (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/Explorer.js:55:27)\n    at async run (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/Explorer.js:35:22)\n    at async cacheWrapper (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/cacheWrapper.js:16:18)\n    at async cacheWrapper (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/cacheWrapper.js:16:18)\n    at async cacheWrapper (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/cacheWrapper.js:16:18)\n    at async Explorer.search (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/cosmiconfig/dist/Explorer.js:27:20)\n    at async loadConfig (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/postcss-loader/dist/utils.js:68:16)\n    at async Object.loader (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/postcss-loader/dist/index.js:54:22)\n    at processResult (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/webpack/lib/NormalModule.js:764:19)\n    at /home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/webpack/lib/NormalModule.js:866:5\n    at /home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at context.callback (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at Object.loader (/home/thiagosoares/Projects/testePHP/teste-desenvolvedor-php/crud-laravel-pleno/node_modules/postcss-loader/dist/index.js:56:7)");
+
+/***/ }),
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: 'import' and 'export' may appear only with 'sourceType: module' (1:0)\nFile was processed with these loaders:\n * ./node_modules/babel-loader/lib/index.js\nYou may need an additional loader to handle the result of these loaders.\n> import './bootstrap';\n| import Alpine from 'alpinejs';\n| window.Alpine = Alpine;");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/sass/app.scss"]();
+/******/ 	
+/******/ })()
+;
